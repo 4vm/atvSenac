1a4e70bcfe5a6f980aee9f22d9def32e69ab9c36
4vm <amadeumrzr@gmail.com>
2020-10-16 20:00:20 -0300

    First commit

---
+++ WelloHorld/WelloHorld/MainPage.xaml.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace WelloHorld
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        async void OnButtonClicked(object sender, EventArgs args)
+        {
+            await input.RelRotateTo(360, 1000);
+            await DisplayAlert("Hey", input.Text + ", Wello Horld \U0001F60E", "Ok");
+        }
+    }
+}
